0fc6ce398b258cad8a87f1ed1f4a782ff4fee73c
Henderson Hood <hendy@hendersonhood.com>
2019-12-28 23:08:25 +0000

    change name from "Henderson" to "Hendy"

--- hendy.c
+++ hendy.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 
-char *names[] = {"Henderson", "Kenneth", "Elvis", "Fernando", "Clee", "Dian Brown"};
+char *names[] = {"Hendy", "Kenneth", "Elvis", "Fernando", "Clee", "Dian Brown"};
 char name[40];
 
 int count = sizeof(names) / sizeof(names[0]);
